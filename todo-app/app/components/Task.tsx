3b1e844b72bf16672440e46c08dcdb660c8e346c
Amaya Wickramasinghe <amawickramasinghe2001@gmail.com>
2023-06-04 19:53:22 +0530

    Can be added a new task, edit the task and delete task

--- todo-app/app/components/Task.tsx
+++ todo-app/app/components/Task.tsx
@@ -1,16 +1,90 @@
+"use client";
+
 import { ITask } from "@/types/tasks";
-import React from "react";
+import { FiEdit } from "react-icons/fi";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { FormEventHandler, useState } from "react";
+import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
+import { deleteTodo, editTodo } from "@/api";
 
 interface TaskProps {
   task: ITask;
 }
 
 const Task: React.FC<TaskProps> = ({ task }) => {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    });
+
+    setOpenModalEdit(false);
+    router.refresh();
+  };
+
+  const handleDeleteTask = async (id: string) => {
+    await deleteTodo(id);
+    setOpenModalDeleted(false);
+    router.refresh();
+  };
+
   return (
     <tr key={task.id}>
-      <td>{task.text}</td>
+      <td className="w-full">{task.text}</td>
 
-      <td>Blue</td>
+      <td className="flex gap-5">
+        <span style={{ cursor: "pointer" }}>
+          <FiEdit
+            onClick={() => setOpenModalEdit(true)}
+            color="blue"
+            size={25}
+          />
+        </span>
+        <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className="font-bold text-lg">Edit Task</h3>
+
+            <div className="modal-action">
+              <input
+                type="text"
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </div>
+          </form>
+        </Modal>
+        <span style={{ cursor: "pointer" }}>
+          <FaRegTrashAlt
+            onClick={() => setOpenModalDeleted(true)}
+            color="red"
+            size={25}
+          />
+        </span>
+        <Modal modalOpen={openModalDeleted} setModalOpen={setOpenModalDeleted}>
+          <h3 className="text-lg">
+            Are you sure, you want to delete this task?
+          </h3>
+          <div className="modal-action">
+            <button onClick={() => handleDeleteTask(task.id)} className="btn">
+              Yes
+            </button>
+          </div>
+        </Modal>
+      </td>
     </tr>
   );
 };
